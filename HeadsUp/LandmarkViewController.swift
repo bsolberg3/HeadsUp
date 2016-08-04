c366dd79c5f8a471d1d5c055999812f37982d9b1
Bobby Solberg <bobbysolberg@bobbys-macbook-pro.local>
2016-08-04 09:43:49 -0500

    Finished some layout things

---
+++ HeadsUp/LandmarkViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LandmarkViewController.swift
+//  HeadsUp
+//
+//  Created by Bobby Solberg on 8/4/16.
+//  Copyright © 2016 Bobby Solberg. All rights reserved.
+//
+
+import Cocoa
+
+class LandmarkViewController: UIViewController {
+
+}
---
+++ HeadsUp/MoviesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MoviesViewController.swift
+//  HeadsUp
+//
+//  Created by Bobby Solberg on 8/4/16.
+//  Copyright © 2016 Bobby Solberg. All rights reserved.
+//
+
+import Cocoa
+
+class MoviesViewController: UIViewController {
+
+}
